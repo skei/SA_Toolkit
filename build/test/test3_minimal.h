bc2c7ab4743aef403b609ff0bda949c78085e054
tor.helge.skei <tor.helge.skei@gmail.com>
2023-04-10 16:58:27 +0200

    ...

--- build/test/test3_minimal.h
+++ build/test/test3_minimal.h
@@ -96,65 +96,6 @@ public:
     registerDefaultExtensions();    
     appendStereoInputPort();
     appendStereoOutputPort();
-    SAT_Parameter* param = new SAT_Parameter("Param1", 0.3);
-    param->setFlag(CLAP_PARAM_IS_MODULATABLE);
-    appendParameter( param );
-    //for (uint32_t i=0; i<5; i++) { appendParameter( new SAT_Parameter("Par",0) ); }
-    return SAT_Plugin::init();
-  }
-  
-  void processAudio(SAT_ProcessContext* AContext) final {
-    float** inputs = AContext->process->audio_inputs[0].data32;
-    float** outputs = AContext->process->audio_outputs[0].data32;
-    uint32_t length = AContext->process->frames_count;
-    SAT_CopyStereoBuffer(outputs,inputs,length);
-    double scale = getParameterValue(0) + getModulationValue(0);
-    scale = SAT_Clamp(scale,0,1);
-    SAT_ScaleStereoBuffer(outputs,scale,length);
-  }
-  
-};
-
-SAT_PLUGIN_ENTRY(myDescriptor,myPlugin)
-
-*/
-
-//----------------------------------------------------------------------
-//
-//----------------------------------------------------------------------
-
-/*
-
-#include "base/sat.h"
-#include "audio/sat_audio_utils.h"
-#include "plugin/sat_entry.h"
-#include "plugin/sat_plugin.h"
-#include "gui/sat_widgets.h"
-
-const clap_plugin_descriptor_t myDescriptor = {
-  .clap_version = CLAP_VERSION,
-  .id           = "me/myPlugin/0.0.1",
-  .name         = "myPlugin",
-  .vendor       = "skei.audio",
-  .url          = "",
-  .manual_url   = "",
-  .support_url  = "",
-  .version      = "0.0.1",
-  .description  = "a nice plugin",
-  .features     = (const char*[]){ CLAP_PLUGIN_FEATURE_AUDIO_EFFECT, nullptr }
-};
-
-class myPlugin
-: public SAT_Plugin {
-  
-public:
-
-  SAT_PLUGIN_DEFAULT_CONSTRUCTOR(myPlugin)
-
-  bool init() final {
-    registerDefaultExtensions();    
-    appendStereoInputPort();
-    appendStereoOutputPort();
     setInitialEditorSize(500,120,1.0);
     SAT_Parameter* param = new SAT_Parameter("Param1", 0.3);
     param->setFlag(CLAP_PARAM_IS_MODULATABLE);
