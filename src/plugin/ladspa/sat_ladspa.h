7627d9f06e5e71850ff432d3da2b6c7bf1c0f4f8
tor.helge.skei <tor.helge.skei@gmail.com>
2023-05-09 20:28:53 +0200

    ...

--- src/plugin/ladspa/sat_ladspa.h
+++ src/plugin/ladspa/sat_ladspa.h
@@ -4,6 +4,17 @@
 
 #include "base/sat.h"
 
+#ifdef SAT_LINUX
+  #include <ladspa.h>
+#endif
+
+#ifdef SAT_WIN32
+  #include "/usr/include/ladspa.h"
+#endif
+
+#define SAT_PLUGIN_LADSPA_MAX_NAME_LENGTH      256
+#define SAT_PLUGIN_LADSPA_MAX_PORT_NAME_LENGTH 64
+
 //----------------------------------------------------------------------
 #endif
 
