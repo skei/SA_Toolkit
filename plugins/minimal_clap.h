c0e9dcf3a6df005e51296071ce6cc5fe207862f9
tor.helge.skei <tor.helge.skei@gmail.com>
2024-02-19 18:41:24 +0100

    low level clap example

--- plugins/minimal_clap.h
+++ plugins/minimal_clap.h
@@ -1,7 +1,6 @@
 
 #include "sat.h"
 #include "plugin/clap/sat_clap_plugin.h"
-#include "plugin/clap/sat_clap_entry.h"
 
 //----------------------------------------------------------------------
 //
@@ -80,16 +79,81 @@ public:
 };
 
 //----------------------------------------------------------------------
-//
+// entry
 //----------------------------------------------------------------------
+
+//#include "plugin/clap/sat_clap_entry.h"
+//SAT_PLUGIN_ENTRY(myDescriptor,myPlugin);
+
+//----------------------------------------------------------------------
+
+#if 0
+
+#include "plugin/clap/sat_clap_entry.h"
 
 void SAT_Register(SAT_Registry* ARegistry) {
   ARegistry->registerDescriptor(&myDescriptor);
 }
 
-//----------
-
 SAT_ClapPlugin* SAT_CreatePlugin(uint32_t AIndex, const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost) {
   if (AIndex == 0) return new myPlugin(ADescriptor,AHost);
   return nullptr;
 }
+
+#endif // 0
+
+//----------------------------------------------------------------------
+
+//------------------------------
+// factory
+//------------------------------
+
+uint32_t get_plugin_count_callback(const struct clap_plugin_factory *factory) {
+  SAT_PRINT;
+  return 1;
+}
+
+const clap_plugin_descriptor_t* get_plugin_descriptor_callback(const struct clap_plugin_factory *factory, uint32_t index) {
+  SAT_PRINT;
+  if (index == 0) return &myDescriptor;
+  return nullptr;
+}
+
+const clap_plugin_t* create_plugin_callback(const struct clap_plugin_factory *factory, const clap_host_t *host, const char *plugin_id) {
+  SAT_PRINT;
+  myPlugin* plugin = new myPlugin(&myDescriptor,host);
+  return plugin->getClapPlugin();
+}
+
+const clap_plugin_factory_t plugin_factory {
+  .get_plugin_count      = get_plugin_count_callback,
+  .get_plugin_descriptor = get_plugin_descriptor_callback,
+  .create_plugin         = create_plugin_callback
+};
+
+//------------------------------
+// entry
+//------------------------------
+
+bool init_callback(const char *plugin_path) {
+  SAT_PRINT;
+  return true;
+}
+
+void deinit_callback(void) {
+  SAT_PRINT;
+}
+
+const void* get_factory_callback(const char* factory_id) {
+  SAT_PRINT;
+  if (strcmp(factory_id,CLAP_PLUGIN_FACTORY_ID) == 0) return &plugin_factory;
+  return nullptr;
+}
+
+//__SAT_EXPORT
+CLAP_EXPORT extern const clap_plugin_entry_t clap_entry {
+  .clap_version = CLAP_VERSION,
+  .init         = init_callback,
+  .deinit       = deinit_callback,
+  .get_factory  = get_factory_callback
+};
