2e682d17b8c4c0381763d202c31f61396dc125f0
tor.helge.skei <tor.helge.skei@gmail.com>
2024-04-09 21:14:02 +0200

    added timing

--- plugins/sa_automata.h
+++ plugins/sa_automata.h
@@ -52,6 +52,7 @@ private:
 
   double                MSpeed                  = 1.0;
   double                MTuning                 = 0.0;
+  double                MTiming                 = 1.0;
 
   double                MTickSize               = 0.0;
   double                MTickRemaining          = 0.0;
@@ -60,24 +61,31 @@ private:
   uint8_t               MStatesBackup[256*256]  = {0};
   uint8_t               MTempBuffer[256*256]    = {0};
 
-  uint32_t              p_width                 = 0.0;
-  uint32_t              p_height                = 0.0;
-  int32_t               p_tuning                = 0.0;
+  uint32_t              p_width                 = 0;
+  uint32_t              p_height                = 0;
+  uint32_t              p_timing_num            = 0;
+  uint32_t              p_timing_denom          = 0;
+  int32_t               p_tuning                = 0;
   double                p_velocity              = 0.0;
-  uint32_t              p_cc1                   = 0.0;
-  uint32_t              p_cc2                   = 0.0;
-  uint32_t              p_cc1_val               = 0.0;
-  uint32_t              p_cc2_val               = 0.0;
+  uint32_t              p_cc1                   = 0;
+  uint32_t              p_cc2                   = 0;
+  uint32_t              p_cc1_val               = 0;
+  uint32_t              p_cc2_val               = 0;
 
   sa_automata_grid*     w_grid                  = nullptr;
   SAT_DragValueWidget*  w_grid_width            = nullptr;
   SAT_DragValueWidget*  w_grid_height           = nullptr;
+  SAT_TextWidget*       w_timing_text           = nullptr;
+  SAT_DragValueWidget*  w_timing_num            = nullptr;
+  SAT_TextWidget*       w_timing_sep            = nullptr;
+  SAT_DragValueWidget*  w_timing_denom          = nullptr;
   SAT_DragValueWidget*  w_tuning                = nullptr;
   SAT_DragValueWidget*  w_velocity              = nullptr;
   SAT_DragValueWidget*  w_cc1                   = nullptr;
   SAT_DragValueWidget*  w_cc2                   = nullptr;
   SAT_DragValueWidget*  w_cc1_val               = nullptr;
   SAT_DragValueWidget*  w_cc2_val               = nullptr;
+
 
 //------------------------------
 public:
@@ -93,14 +101,16 @@ public:
     registerDefaultExtensions();    
     registerExtension(CLAP_EXT_NOTE_PORTS);
     appendNoteOutputPort("Out", CLAP_NOTE_DIALECT_CLAP | CLAP_NOTE_DIALECT_MIDI, CLAP_NOTE_DIALECT_CLAP);
-    appendParameter( new SAT_IntParameter( "Width",    16,    4,   32,    CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
-    appendParameter( new SAT_IntParameter( "Height",   16,    4,   32,    CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
-    appendParameter( new SAT_IntParameter( "Tuning",   0,    -48,  48,    CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
-    appendParameter( new SAT_Parameter(    "Velocity", 0.0,   0.0, 1.0,   CLAP_PARAM_IS_AUTOMATABLE ));
-    appendParameter( new SAT_IntParameter( "CC1",      64,    0,   127,   CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
-    appendParameter( new SAT_IntParameter( "CC2",      65,    0,   127,   CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
-    appendParameter( new SAT_IntParameter( "CC1 Val",  127,   0,   127,   CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
-    appendParameter( new SAT_IntParameter( "CC2 Val",  127,   0,   127,   CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_IntParameter( "Width",        16,   4,   32,    CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_IntParameter( "Height",       16,   4,   32,    CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_IntParameter( "Timing Num",   1,    1,   8,     CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_IntParameter( "Timing Denom", 1,    1,   8,     CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_IntParameter( "Tuning",       0,   -48,  48,    CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_Parameter(    "Velocity",     0.0,  0.0, 1.0,   CLAP_PARAM_IS_AUTOMATABLE ));
+    appendParameter( new SAT_IntParameter( "CC1",          64,   0,   127,   CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_IntParameter( "CC2",          65,   0,   127,   CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_IntParameter( "CC1 Val",      127,  0,   127,   CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
+    appendParameter( new SAT_IntParameter( "CC2 Val",      127,  0,   127,   CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED ));
     #if !defined (SAT_GUI_NOGUI)
       setInitialEditorSize( 420, 560, 1.0 );
     #endif
@@ -122,26 +132,42 @@ public:
     void setupEditorWindow(SAT_Editor* AEditor, SAT_Window* AWindow) final {
       sa_automata_root_widget* root = new sa_automata_root_widget(AWindow);
       AWindow->setRootWidget(root);
-      w_grid        = new sa_automata_grid(    SAT_Rect( 10, 50, 400,400), 16, 16, MStates);
-      w_grid_width  = new SAT_DragValueWidget( SAT_Rect( 10,480, 80, 20),"Width");
-      w_grid_height = new SAT_DragValueWidget( SAT_Rect( 10,505, 80, 20),"Height");
-      w_tuning      = new SAT_DragValueWidget( SAT_Rect(100,480, 80, 20),"Tuning");
-      w_velocity    = new SAT_DragValueWidget( SAT_Rect(100,505, 80, 20),"Velocity");
-      w_cc1         = new SAT_DragValueWidget( SAT_Rect(190,480, 80, 20),"CC1");
-      w_cc2         = new SAT_DragValueWidget( SAT_Rect(190,505, 80, 20),"CC2");
-      w_cc1_val     = new SAT_DragValueWidget( SAT_Rect(280,480, 80, 20),"CC1 Val");
-      w_cc2_val     = new SAT_DragValueWidget( SAT_Rect(280,505, 80, 20),"CC2 Val");
+
+      w_grid          = new sa_automata_grid(     SAT_Rect( 10, 10, 400,400), 16, 16, MStates);
+      w_grid_width    = new SAT_DragValueWidget(  SAT_Rect( 10,440, 80, 20 ), "Width");
+      w_grid_height   = new SAT_DragValueWidget(  SAT_Rect( 10,465, 80, 20 ), "Height");
+
+      w_timing_text   = new SAT_TextWidget(       SAT_Rect( 10,500, 35, 20 ), "Timing");
+      w_timing_num    = new SAT_DragValueWidget(  SAT_Rect( 45,500, 20, 20 ), "");
+      w_timing_sep    = new SAT_TextWidget(       SAT_Rect( 68,500, 5, 20 ), "/");
+      w_timing_denom  = new SAT_DragValueWidget(  SAT_Rect( 75,500, 20, 20 ), "");
+
+      w_tuning        = new SAT_DragValueWidget(  SAT_Rect(100,440, 80, 20 ), "Tuning");
+      w_velocity      = new SAT_DragValueWidget(  SAT_Rect(100,465, 80, 20 ), "Velocity");
+      w_cc1           = new SAT_DragValueWidget(  SAT_Rect(190,440, 80, 20 ), "CC1");
+      w_cc2           = new SAT_DragValueWidget(  SAT_Rect(190,465, 80, 20 ), "CC2");
+      w_cc1_val       = new SAT_DragValueWidget(  SAT_Rect(280,440, 80, 20 ), "CC1 Val");
+      w_cc2_val       = new SAT_DragValueWidget(  SAT_Rect(280,465, 80, 20 ), "CC2 Val");
+
       w_tuning->setBackgroundColor( SAT_DarkRed );
       w_velocity->setBackgroundColor( SAT_DarkRed );
+      w_timing_num->setDrawText(false);
+      w_timing_denom->setDrawText(false);
       w_cc1->setBackgroundColor( SAT_DarkGreen );
       w_cc1_val->setBackgroundColor( SAT_DarkGreen );
       w_cc2->setBackgroundColor( SAT_DarkBlue );
       w_cc2_val->setBackgroundColor( SAT_DarkBlue );
+
       root->WGridWidth  = w_grid_width;
       root->WGridHeight = w_grid_height;
       root->WGrid       = w_grid;
+
       root->appendChildWidget(w_grid_width);
       root->appendChildWidget(w_grid_height);
+      root->appendChildWidget(w_timing_text);
+      root->appendChildWidget(w_timing_num);
+      root->appendChildWidget(w_timing_sep);
+      root->appendChildWidget(w_timing_denom);
       root->appendChildWidget(w_grid);
       root->appendChildWidget(w_tuning);
       root->appendChildWidget(w_velocity);
@@ -149,14 +175,18 @@ public:
       root->appendChildWidget(w_cc2);
       root->appendChildWidget(w_cc1_val);
       root->appendChildWidget(w_cc2_val);
+
       AEditor->connect(w_grid_width,  getParameter(0));
       AEditor->connect(w_grid_height, getParameter(1));
-      AEditor->connect(w_tuning,      getParameter(2));
-      AEditor->connect(w_velocity,    getParameter(3));
-      AEditor->connect(w_cc1,         getParameter(4));
-      AEditor->connect(w_cc2,         getParameter(5));
-      AEditor->connect(w_cc1_val,     getParameter(6));
-      AEditor->connect(w_cc2_val,     getParameter(7));
+      AEditor->connect(w_timing_num,  getParameter(2));
+      AEditor->connect(w_timing_denom,getParameter(3));
+      AEditor->connect(w_tuning,      getParameter(4));
+      AEditor->connect(w_velocity,    getParameter(5));
+      AEditor->connect(w_cc1,         getParameter(6));
+      AEditor->connect(w_cc2,         getParameter(7));
+      AEditor->connect(w_cc1_val,     getParameter(8));
+      AEditor->connect(w_cc2_val,     getParameter(9));
+
     }
   #endif
 
@@ -169,14 +199,16 @@ public:
     uint32_t index = event->param_id;
     double value = event->value;
     switch (index) {
-      case 0: p_width     = value;  break;
-      case 1: p_height    = value;  break;
-      case 2: p_tuning    = value;  break;
-      case 3: p_velocity  = value;  break;
-      case 4: p_cc1       = value;  break;
-      case 5: p_cc2       = value;  break;
-      case 6: p_cc1_val   = value;  break;
-      case 7: p_cc2_val   = value;  break;
+      case 0: p_width         = value;  break;
+      case 1: p_height        = value;  break;
+      case 2: p_timing_num    = value;  break;
+      case 3: p_timing_denom  = value;  break;
+      case 4: p_tuning        = value;  break;
+      case 5: p_velocity      = value;  break;
+      case 6: p_cc1           = value;  break;
+      case 7: p_cc2           = value;  break;
+      case 8: p_cc1_val       = value;  break;
+      case 9: p_cc2_val       = value;  break;
     }
     return true;
   }
@@ -215,11 +247,16 @@ public:
   //----------
 
   void processStereoSample(sat_sample_t* spl0, sat_sample_t* spl1) final {
+
+    if ((p_timing_num > 0) && (p_timing_denom > 0)) {
+      MTiming = (double)p_timing_num / (double)p_timing_denom;
+    }
+
     if (MIsPlaying) {
       MTickRemaining -= 1.0;
       if (MTickRemaining <= 0.0) {
         //MTickRemaining = SAT_Fract(MTickRemaining);
-        MTickRemaining += MTickSize; // = MTickSize - MTickRemaining;
+        MTickRemaining += MTickSize * MTiming;
         handleTick();
       }
     }
@@ -303,7 +340,7 @@ public:
     event.header.size     = sizeof(clap_event_note_t);
     event.header.time     = AOffset;
     event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    event.header.type     = CLAP_EVENT_NOTE_ON;
+    event.header.type     = CLAP_EVENT_NOTE_OFF;
     event.header.flags    = 0;
     event.note_id         = -1;
     event.port_index      = 0;
@@ -319,7 +356,7 @@ public:
   void sendMidiCCEvent(int32_t ACC, double AValue, uint32_t AOffset) {
     clap_event_midi_t midi_event;
     midi_event.header.size     = sizeof(clap_event_midi_t);
-    midi_event.header.time     = 0;//AOffset;
+    midi_event.header.time     = AOffset;
     midi_event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
     midi_event.header.type     = CLAP_EVENT_MIDI;
     midi_event.header.flags    = 0;
@@ -334,167 +371,44 @@ public:
   //----------
 
   void killAllNotes() {
-
-    // // notes
-
-    // clap_event_note_t note_event;
-    // note_event.header.size     = sizeof(clap_event_note_t);
-    // note_event.header.time     = 0;
-    // note_event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // note_event.header.type     = CLAP_EVENT_NOTE_OFF;
-    // note_event.header.flags    = 0;
-    // note_event.note_id         = -1;
-    // note_event.port_index      = 0;
-    // note_event.channel         = 0;
-    // note_event.key             = y;
-    // note_event.velocity        = 0;
-    // sendEvent((clap_event_header_t*)&note_event);
-
-    for (uint32_t y=0; y<127; y++) {
-      sendNoteOffEvent(y,0);
-    }
-
-    // cc1
-
-    // clap_event_midi_t midi_event;
-    // midi_event.header.size     = sizeof(clap_event_midi_t);
-    // midi_event.header.time     = 0;//AOffset;
-    // midi_event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // midi_event.header.type     = CLAP_EVENT_MIDI;
-    // midi_event.header.flags    = 0;
-    // midi_event.port_index      = 0;
-    // midi_event.data[0]         = SAT_MIDI_CONTROL_CHANGE + 0x00;
-    // midi_event.data[1]         = p_cc1;
-    // midi_event.data[2]         = 0;
-    // sendEvent((clap_event_header_t*)&midi_event);
-
+    for (uint32_t y=0; y<127; y++) sendNoteOffEvent(y,0);
     sendMidiCCEvent(p_cc1,0,0);
-
-    // cc2
-
-    // //clap_event_midi_t midi_event;
-    // midi_event.header.size     = sizeof(clap_event_midi_t);
-    // midi_event.header.time     = 0;//AOffset;
-    // midi_event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // midi_event.header.type     = CLAP_EVENT_MIDI;
-    // midi_event.header.flags    = 0;
-    // midi_event.port_index      = 0;
-    // midi_event.data[0]         = SAT_MIDI_CONTROL_CHANGE + 0x00;
-    // midi_event.data[1]         = p_cc2;
-    // midi_event.data[2]         = 0;
-    // sendEvent((clap_event_header_t*)&midi_event);
-
     sendMidiCCEvent(p_cc2,0,0);
-
   }
 
   //----------
 
   void sendNoteOn(int32_t AX, int32_t AY, uint32_t AOffset) {
-    //SAT_Print("x %i y %i ofs %i\n",AX,AY,AOffset);
-    // clap_event_note_t event;
-    // event.header.size     = sizeof(clap_event_note_t);
-    // event.header.time     = AOffset;
-    // event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // event.header.type     = CLAP_EVENT_NOTE_ON;
-    // event.header.flags    = 0;
-    // event.note_id         = -1;
-    // event.port_index      = 0;
-    // event.channel         = 0;
-    // event.key             = 60 + AY + p_tuning;
-    // event.velocity        = p_velocity;
-    // sendEvent((clap_event_header_t*)&event);
     sendNoteOnEvent( 60 + AY + p_tuning, p_velocity, AOffset );
   }
 
   //----------
 
   void sendNoteOff(int32_t AX, int32_t AY, uint32_t AOffset) {
-    //SAT_Print("x %i y %i ofs %i\n",AX,AY,AOffset);
-    // clap_event_note_t event;
-    // event.header.size     = sizeof(clap_event_note_t);
-    // event.header.time     = AOffset;
-    // event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // event.header.type     = CLAP_EVENT_NOTE_OFF;
-    // event.header.flags    = 0;
-    // event.note_id         = -1;
-    // event.port_index      = 0;
-    // event.channel         = 0;
-    // event.key             = 60 + AY + p_tuning;
-    // event.velocity        = 0.0;
-    // sendEvent((clap_event_header_t*)&event);
     sendNoteOffEvent( 60 + AY + p_tuning, AOffset );
   }
 
   //----------
 
   void sendCC1On(int32_t AX, int32_t AY, uint32_t AOffset) {
-    //SAT_Print("x %i y %i ofs %i\n",AX,AY,AOffset);
-    // clap_event_midi_t event;
-    // event.header.size     = sizeof(clap_event_midi_t);
-    // event.header.time     = AOffset;
-    // event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // event.header.type     = CLAP_EVENT_MIDI;
-    // event.header.flags    = 0;
-    // event.port_index      = 0;
-    // event.data[0]         = SAT_MIDI_CONTROL_CHANGE + 0x00;
-    // event.data[1]         = p_cc1;
-    // event.data[2]         = p_cc1_val;
-    // sendEvent((clap_event_header_t*)&event);
     sendMidiCCEvent(p_cc1,p_cc1_val,AOffset);
   }
 
   //----------
 
   void sendCC1Off(int32_t AX, int32_t AY, uint32_t AOffset) {
-    //SAT_Print("x %i y %i ofs %i\n",AX,AY,AOffset);
-    // clap_event_midi_t event;
-    // event.header.size     = sizeof(clap_event_midi_t);
-    // event.header.time     = AOffset;
-    // event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // event.header.type     = CLAP_EVENT_MIDI;
-    // event.header.flags    = 0;
-    // event.port_index      = 0;
-    // event.data[0]         = SAT_MIDI_CONTROL_CHANGE + 0x00;
-    // event.data[1]         = p_cc1;
-    // event.data[2]         = 0;//p_cc1_val;
-    // sendEvent((clap_event_header_t*)&event);
     sendMidiCCEvent(p_cc1,0,AOffset);
   }
 
   //----------
 
   void sendCC2On(int32_t AX, int32_t AY, uint32_t AOffset) {
-    //SAT_Print("x %i y %i ofs %i\n",AX,AY,AOffset);
-    // clap_event_midi_t event;
-    // event.header.size     = sizeof(clap_event_midi_t);
-    // event.header.time     = AOffset;
-    // event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // event.header.type     = CLAP_EVENT_MIDI;
-    // event.header.flags    = 0;
-    // event.port_index      = 0;
-    // event.data[0]         = SAT_MIDI_CONTROL_CHANGE + 0x00;
-    // event.data[1]         = p_cc2;
-    // event.data[2]         = p_cc2_val;
-    // sendEvent((clap_event_header_t*)&event);
     sendMidiCCEvent(p_cc2,p_cc2_val,AOffset);
   }
 
   //----------
 
   void sendCC2Off(int32_t AX, int32_t AY, uint32_t AOffset) {
-    //SAT_Print("x %i y %i ofs %i\n",AX,AY,AOffset);
-    // clap_event_midi_t event;
-    // event.header.size     = sizeof(clap_event_midi_t);
-    // event.header.time     = AOffset;
-    // event.header.space_id = CLAP_CORE_EVENT_SPACE_ID;
-    // event.header.type     = CLAP_EVENT_MIDI;
-    // event.header.flags    = 0;
-    // event.port_index      = 0;
-    // event.data[0]         = SAT_MIDI_CONTROL_CHANGE + 0x00;
-    // event.data[1]         = p_cc2;
-    // event.data[2]         = 0;//p_cc2_val;
-    // sendEvent((clap_event_header_t*)&event);
     sendMidiCCEvent(p_cc2,0,AOffset);
   }
 
