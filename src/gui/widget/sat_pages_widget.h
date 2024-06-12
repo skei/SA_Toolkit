9737f54675c17f642cdd1e672f542c60e4a69b91
tor.helge.skei <tor.helge.skei@gmail.com>
2024-06-12 13:19:12 +0200

    add pages widget

---
+++ src/gui/widget/sat_pages_widget.h
@@ -0,0 +1,78 @@
+#ifndef sat_pages_widget_included
+#define sat_pages_widget_included
+//----------------------------------------------------------------------
+
+#include "sat.h"
+#include "gui/widget/sat_visual_widget.h"
+
+//----------------------------------------------------------------------
+//
+//
+//
+//----------------------------------------------------------------------
+
+class SAT_PagesWidget
+: public SAT_VisualWidget {
+
+//------------------------------
+protected:
+//------------------------------
+
+  uint32_t MActivePage = 0;
+
+//------------------------------
+public:
+//------------------------------
+
+  SAT_PagesWidget(SAT_Rect ARect)
+  : SAT_VisualWidget(ARect) {
+    setName("SAT_PagesWidget");
+  }
+
+  //----------
+
+  virtual ~SAT_PagesWidget() {
+  }
+
+//------------------------------
+public:
+//------------------------------
+
+  uint32_t getActivePage() {
+    return MActivePage;
+  }
+
+  //----------
+
+  int32_t appendPage(SAT_Widget* APage) {
+    //SAT_Print("index %i\n",getNumChildWidgets());
+    //APage->State.active = false;
+    APage->setActive(false);
+    APage->setVisible(false);
+    return appendChild(APage)->getIndex();
+  }
+
+  //----------
+
+  void setPage(int32_t APage) {
+    //SAT_Print("page %i\n",APage);
+    int num = getNumChildren();//MChildren.size();
+    if (APage >= num) APage = num - 1;
+    for (int32_t i=0; i<num; i++) {
+      SAT_Widget* wdg = getChild(i);//MChildren[i];
+      if (i == APage) {
+        wdg->setActive(true);
+        wdg->setVisible(true);
+      }
+      else {
+        wdg->setActive(false);
+        wdg->setVisible(false);
+      }
+    }
+    MActivePage = APage;
+  }
+
+};
+
+//----------------------------------------------------------------------
+#endif
