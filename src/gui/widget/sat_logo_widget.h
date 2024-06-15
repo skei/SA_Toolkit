1bc3177b6aef9a0f7a4f48f0a1d20e33559d57cf
tor.helge.skei <tor.helge.skei@gmail.com>
2024-06-15 11:54:12 +0200

    #ifdef-away svg things if not using nanovg (for now)

--- src/gui/widget/sat_logo_widget.h
+++ src/gui/widget/sat_logo_widget.h
@@ -21,12 +21,14 @@ private:
 //------------------------------
 
   bool        MDrawLogo     = true;
-  NSVGimage*  MLogoImage    = nullptr;
   SAT_Color   MLogoColor    = SAT_White;
   double      MLogoWidth    = 1.0;
   char*       MBuffer       = nullptr;
-
   SAT_Rect    MLogoOffset   = {0,0,0,0};
+
+  #ifdef SAT_PAINTER_NANOVG
+    NSVGimage*  MLogoImage    = nullptr;
+  #endif
 
 //------------------------------
 public:
@@ -37,18 +39,22 @@ public:
     setName("SAT_LogoWidget");
     setFillBackground(false);
     setDrawBorder(false);
-    MBuffer = (char*)malloc(logo_svg_size + 1);
-    memcpy(MBuffer,logo_svg,logo_svg_size);
-    MBuffer[logo_svg_size] = 0;
-    MLogoImage = nsvgParse(MBuffer, "px", 96);
-    //SAT_Assert(MLogoImage);
+    #ifdef SAT_PAINTER_NANOVG
+      MBuffer = (char*)malloc(logo_svg_size + 1);
+      memcpy(MBuffer,logo_svg,logo_svg_size);
+      MBuffer[logo_svg_size] = 0;
+      MLogoImage = nsvgParse(MBuffer, "px", 96);
+      //SAT_Assert(MLogoImage);
+    #endif
   }
 
   //----------
 
   virtual ~SAT_LogoWidget() {
-    if (MBuffer) free(MBuffer);
-    nsvgDelete(MLogoImage);
+    #ifdef SAT_PAINTER_NANOVG
+      if (MBuffer) free(MBuffer);
+      nsvgDelete(MLogoImage);
+    #endif
   }
 
 //------------------------------
@@ -66,56 +72,57 @@ public:
 
   virtual void drawLogo(SAT_PaintContext* AContext) {
     SAT_Assert(AContext);
+    #ifdef SAT_PAINTER_NANOVG
+      if (MDrawLogo) {
 
-    if (MDrawLogo) {
+        double S = getWindowScale();
 
-      double S = getWindowScale();
+        SAT_Painter* painter = AContext->painter;
+        SAT_Assert(painter);
 
-      SAT_Painter* painter = AContext->painter;
-      SAT_Assert(painter);
+        SAT_Rect mrect = getRect();
+        SAT_Rect ofs = MLogoOffset;
+        ofs.scale(S);
+        mrect.shrink(ofs);
 
-      SAT_Rect mrect = getRect();
-      SAT_Rect ofs = MLogoOffset;
-      ofs.scale(S);
-      mrect.shrink(ofs);
+        if (!MLogoImage) return;
+        if (MLogoImage->width <= 0.0 ) return;
+        if (MLogoImage->height <= 0.0 ) return;
 
-      if (!MLogoImage) return;
-      if (MLogoImage->width <= 0.0 ) return;
-      if (MLogoImage->height <= 0.0 ) return;
+        painter->setDrawColor(MLogoColor);
+        painter->setLineWidth(MLogoWidth*S);
+        painter->setLineCap(NVG_ROUND);       // NVG_BUTT (default), NVG_ROUND, NVG_SQUARE
+        //painter->setLineJoin(NVG_MITER);    // NVG_MITER (default), NVG_ROUND, NVG_BEVEL
 
-      painter->setDrawColor(MLogoColor);
-      painter->setLineWidth(MLogoWidth*S);
-      painter->setLineCap(NVG_ROUND);       // NVG_BUTT (default), NVG_ROUND, NVG_SQUARE
-      //painter->setLineJoin(NVG_MITER);    // NVG_MITER (default), NVG_ROUND, NVG_BEVEL
+        double wscale  = (mrect.w / MLogoImage->width);
+        double hscale  = (mrect.h / MLogoImage->height);
+        
+        double scale = wscale;
+        if (hscale < wscale) scale = hscale;
 
-      double wscale  = (mrect.w / MLogoImage->width);
-      double hscale  = (mrect.h / MLogoImage->height);
-      
-      double scale = wscale;
-      if (hscale < wscale) scale = hscale;
-
-      for (NSVGshape* shape = MLogoImage->shapes; shape != NULL; shape = shape->next) {
-        for (NSVGpath* path = shape->paths; path != NULL; path = path->next) {
-          //painter->setLineWidth( S * shape->strokeWidth );
-          for (int i = 0; i < path->npts-1; i += 3) {
-            float* p = &path->pts[i*2];
-            double x1 = mrect.x + (p[0] * scale);
-            double y1 = mrect.y + (p[1] * scale);
-            double x2 = mrect.x + (p[2] * scale);
-            double y2 = mrect.y + (p[3] * scale);
-            double x3 = mrect.x + (p[4] * scale);
-            double y3 = mrect.y + (p[5] * scale);
-            double x4 = mrect.x + (p[6] * scale);
-            double y4 = mrect.y + (p[7] * scale);
-            painter->drawCurveBezier(x1,y1, x4,y4, x2,y2, x3,y3);
+        for (NSVGshape* shape = MLogoImage->shapes; shape != NULL; shape = shape->next) {
+          for (NSVGpath* path = shape->paths; path != NULL; path = path->next) {
+            //painter->setLineWidth( S * shape->strokeWidth );
+            for (int i = 0; i < path->npts-1; i += 3) {
+              float* p = &path->pts[i*2];
+              double x1 = mrect.x + (p[0] * scale);
+              double y1 = mrect.y + (p[1] * scale);
+              double x2 = mrect.x + (p[2] * scale);
+              double y2 = mrect.y + (p[3] * scale);
+              double x3 = mrect.x + (p[4] * scale);
+              double y3 = mrect.y + (p[5] * scale);
+              double x4 = mrect.x + (p[6] * scale);
+              double y4 = mrect.y + (p[7] * scale);
+              painter->drawCurveBezier(x1,y1, x4,y4, x2,y2, x3,y3);
+            }
           }
         }
+
+        // reset
+        painter->setLineCap(NVG_BUTT);
+
       }
-
-      // reset
-      painter->setLineCap(NVG_BUTT);
-
-    }
+    #endif // SAT_PAINTER_NANOVG
   }
 
 //------------------------------
