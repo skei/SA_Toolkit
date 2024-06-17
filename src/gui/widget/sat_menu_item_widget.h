789d9fdc4eab1c571287a2859fdf84c8a3deec8a
tor.helge.skei <tor.helge.skei@gmail.com>
2024-06-17 19:25:45 +0200

    cleanup

--- src/gui/widget/sat_menu_item_widget.h
+++ src/gui/widget/sat_menu_item_widget.h
@@ -49,7 +49,7 @@ public:
 //------------------------------
 
   void on_widget_mouse_click(int32_t AXpos, int32_t AYpos, uint32_t AButton, uint32_t AState, uint32_t ATime) override {
-    SAT_TRACE;
+    //SAT_TRACE;
     int32_t index = getIndex();
     SAT_Rect rect = getRect();
     if (!rect.contains(AXpos,AYpos)) {
@@ -69,20 +69,20 @@ public:
   //----------
 
   void on_widget_key_press(uint32_t AKey, uint32_t AChar, uint32_t AState, uint32_t ATime) override {
-    SAT_TRACE;
+    //SAT_TRACE;
     // if ESC
     do_widget_notify(this,-1);
   }
 
   void on_widget_enter(SAT_Widget* AFrom, int32_t AXpos, int32_t AYpos, uint32_t ATime) {
-    SAT_TRACE;
+    //SAT_TRACE;
     setBackgroundColor(MHoverBackgroundColor);
     setTextColor(MHoverTextColor);
     do_widget_redraw(this,0);
   }
 
   void on_widget_leave(SAT_Widget* AFrom, int32_t AXpos, int32_t AYpos, uint32_t ATime) {
-    SAT_TRACE;
+    //SAT_TRACE;
     setBackgroundColor(MUnHoverBackgroundColor);
     setTextColor(MUnHoverTextColor);
     do_widget_redraw(this,0);
