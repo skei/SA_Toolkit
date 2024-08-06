581461dfd8b757308a47380f719931f6a0ba8468
[Dirk Schiller] <[dirk.schiller@native-instruments.com]>
2024-08-06 06:15:03 +0200

    Test all builds on macOS

--- build/build.cpp
+++ build/build.cpp
@@ -10,9 +10,9 @@
   #define SAT_PRINT_SOCKET
 #endif
 
-//#define SAT_DEBUG_WINDOW
-//#define SAT_DEBUG_OBSERVER
-//#define SAT_WINDOW_BUFFERED
+// #define SAT_DEBUG_WINDOW
+// #define SAT_DEBUG_OBSERVER
+// #define SAT_WINDOW_BUFFERED
 
 //----------------------------------------------------------------------
 //
@@ -21,9 +21,9 @@
 //----------------------------------------------------------------------
 
 
-//#include "../plugins/test/exe.h"
-//#include "../plugins/test/gain.h"
-//#include "../plugins/test/voices.h"
+// #include "../plugins/test/exe.h"
+// #include "../plugins/test/gain.h"
+// #include "../plugins/test/voices.h"
 
 //----------
 
@@ -37,15 +37,14 @@
 //----------------------------------------------------------------------
 
 
-
-//#include "../plugins/sa_automata.h"
-//#include "../plugins/sa_botage.h"
-//#include "../plugins/sa_bulum.h"
-//#include "../plugins/sa_cred.h"
+// #include "../plugins/sa_automata.h"
+// #include "../plugins/sa_botage.h"
+// #include "../plugins/sa_bulum.h"
+// #include "../plugins/sa_cred.h"
 #include "../plugins/sa_demo.h"
-//#include "../plugins/sa_mael.h"
-//#include "../plugins/sa_ports.h"
-//#include "../plugins/sa_tyr.h"
+// #include "../plugins/sa_mael.h"
+// #include "../plugins/sa_ports.h"
+// #include "../plugins/sa_tyr.h"
 
 
 //----------------------------------------------------------------------
@@ -55,28 +54,28 @@
 //----------------------------------------------------------------------
 
 
-//#include "../plugins/ports/sa_compciter.h"
-//#include "../plugins/ports/sa_event_horizon.h"
-//#include "../plugins/ports/sa_exciter.h"
-//#include "../plugins/ports/sa_exciter2.h"
-//#include "../plugins/ports/sa_freeverb.h"
-//#include "../plugins/ports/sa_hall_reverb.h"
-//#include "../plugins/ports/sa_hrtf.h"
-//#include "../plugins/ports/sa_iirfilter.h"
-//#include "../plugins/ports/sa_large_room_reverb.h"
-//#include "../plugins/ports/sa_limiter.h"
-//#include "../plugins/ports/sa_phaser.h"
-//#include "../plugins/ports/sa_pitch.h"
-//#include "../plugins/ports/sa_saturation.h"
-//#include "../plugins/ports/sa_small_room_reverb.h"
-//#include "../plugins/ports/sa_sonic_maximizer.h"
-//#include "../plugins/ports/sa_stretch.h"
-//#include "../plugins/ports/sa_tilteq.h"
-//#include "../plugins/ports/sa_transient_killer.h"
+// #include "../plugins/ports/sa_compciter.h"
+// #include "../plugins/ports/sa_event_horizon.h"
+// #include "../plugins/ports/sa_exciter.h"
+// #include "../plugins/ports/sa_exciter2.h"
+// #include "../plugins/ports/sa_freeverb.h"
+// #include "../plugins/ports/sa_hall_reverb.h"
+// #include "../plugins/ports/sa_hrtf.h"
+// #include "../plugins/ports/sa_iirfilter.h"
+// #include "../plugins/ports/sa_large_room_reverb.h"
+// #include "../plugins/ports/sa_limiter.h"
+// #include "../plugins/ports/sa_phaser.h"
+// #include "../plugins/ports/sa_pitch.h"
+// #include "../plugins/ports/sa_saturation.h"
+// #include "../plugins/ports/sa_small_room_reverb.h"
+// #include "../plugins/ports/sa_sonic_maximizer.h"
+// #include "../plugins/ports/sa_stretch.h"
+// #include "../plugins/ports/sa_tilteq.h"
+// #include "../plugins/ports/sa_transient_killer.h"
 
 //----------
 
-//#include "../plugins/ports/sa_reverb.h"
+// #include "../plugins/ports/sa_reverb.h"
 
 
 //----------------------------------------------------------------------
@@ -88,6 +87,4 @@
 
 // -t, #define SAT_INCLUDE_TESTS
 
-//#include "../test/sat_all_tests.h"
-
-
+// #include "../test/sat_all_tests.h"
