4d391c46cfc56f3ad429ae14d0147801b8f92904
tor.helge.skei <tor.helge.skei@gmail.com>
2025-04-29 22:07:03 +0200

    trying out gain-adjustment-metering clap extension

--- plugins/sa_demo.cpp
+++ plugins/sa_demo.cpp
@@ -158,7 +158,11 @@ public:
 //------------------------------
 
   bool init() final {
-    registerDefaultExtensions();    
+    registerDefaultExtensions();
+
+    registerExtension(CLAP_EXT_MINI_CURVE_DISPLAY);
+    registerExtension(CLAP_EXT_GAIN_ADJUSTMENT_METERING);
+
     appendStereoAudioInputPort("In");
     appendStereoAudioOutputPort("Out");
     MProcessor = new sa_demo_processor(this);
@@ -167,7 +171,17 @@ public:
     #ifndef SAT_NO_GUI
       setInitialEditorSize(EDITOR_WIDTH,EDITOR_HEIGHT,EDITOR_SCALE,true);
     #endif
-    return SAT_Plugin::init();
+    //return SAT_Plugin::init();
+    bool result = SAT_Plugin::init();
+    printSupportedHostExtensions();
+    SAT_Host* host = getHost();
+    if (result) {
+      if (host->ext.mini_curve_display) {
+        host->ext.mini_curve_display->set_dynamic(getClapHost(),true);
+        //host->ext.mini_curve_display->changed(getClapHost(),CLAP_MINI_CURVE_DISPLAY_CURVE_CHANGED);
+      }
+    }
+    return result;
   }
   
   //----------
@@ -194,7 +208,64 @@ public:
   //   //   voices->setVoiceState(i,state);
   //   // }
   //   // voices->do_Widget_redraw(voices);
-  // }  
+  // }
+
+//------------------------------
+public: // gain-adjustment-metering
+//------------------------------
+
+  double gain_adjustment_metering_get() final {
+    double value = SAT_RandomRange(-30.0,30.0);
+    //SAT_PRINT("value: %.3f\n",value);
+    return value;
+  }
+
+//------------------------------
+public: // mini-curve-display
+//------------------------------
+
+  uint32_t mini_curve_display_get_curve_count() final {
+    //SAT_PRINT("return 1\n");
+    return 1;
+  }
+
+  //----------
+
+  uint32_t mini_curve_display_render(clap_mini_curve_display_curve_data_t *curves, uint32_t curves_size) final {
+    //SAT_PRINT("curves_size: %i\n",curves_size);
+    //if (curves_size == 1) {
+    for (uint32_t curve=0; curve<curves_size; curve++) {
+      SAT_Host* host = getHost();
+      clap_mini_curve_display_curve_hints_t hints = {0};
+      bool result = host->ext.mini_curve_display->get_hints(getClapHost(),CLAP_MINI_CURVE_DISPLAY_CURVE_KIND_GAIN_RESPONSE,&hints);
+      if (result) {
+        // never printed, because bitwig/linux get_hints() returns false
+        //SAT_PRINT("curve: %i : min %.2f,%.2f max %.2f,%.2f\n",curve,hints.x_min,hints.y_min,hints.x_max,hints.y_max);
+      }
+      uint32_t count = curves[curve].values_count;
+      for (uint32_t i=0; i<count; i++) {
+        curves[curve].curve_kind = CLAP_MINI_CURVE_DISPLAY_CURVE_KIND_TIME_SERIES;
+        curves[curve].values[i] = SAT_RandomRangeInt(1,0xfffe);
+      }
+    }
+    return curves_size;
+  }
+
+  //----------
+
+  void mini_curve_display_set_observed(bool is_observed) final {
+    //SAT_PRINT("is_observed: %s\n",is_observed ? "true" : "false");
+  }
+
+  //----------
+
+  bool mini_curve_display_get_axis_name(uint32_t curve_index, char *x_name, char *y_name, uint32_t name_capacity) final {
+    //SAT_PRINT("curve_index: %i name_capacity: %i\n",curve_index,name_capacity);
+    strcpy(x_name,"X");
+    strcpy(y_name,"Y");
+    return true;
+  }
+
 
 };
 
